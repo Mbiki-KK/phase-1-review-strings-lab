263f25762b6db098732e96ba58d7c7ee99c2a590
Mbiki-KK <wawerumbiki@gmail.com>
2023-03-22 11:19:01 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase()
 
+currentUser.slice(0,1)
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
 
